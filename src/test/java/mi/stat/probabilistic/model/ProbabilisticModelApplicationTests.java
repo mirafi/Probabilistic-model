8f85a4879997d66d942f2b60bd8de3731aef8274
MI Rafi <rafi101010@gmail.com>
2023-08-01 03:20:36 +0300

    clean up

--- src/test/java/mi/stat/probabilistic/model/ProbabilisticModelApplicationTests.java
+++ src/test/java/mi/stat/probabilistic/model/ProbabilisticModelApplicationTests.java
@@ -1,12 +1,11 @@
 package mi.stat.probabilistic.model;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+
 class ProbabilisticModelApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
